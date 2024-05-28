8121c535ad4e2891c2b64a4ef814b5b8379b3a96
tomoya143y <165997782+tomoya143y@users.noreply.github.com>
2024-05-28 22:50:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const firstCard_detailContainer = document.getElementsByClassName("card_detailCo
 const firstCardDetailsBox = document.getElementsByClassName("card_detailsBox")[0];
 const firstCardDetails = document.getElementsByClassName("card_details")[0];
 const firstEditDetails = document.getElementsByClassName("edit_detail")[0];
+const firstCardAttachment = document.getElementsByClassName("add")[0];
 
 
 //カード内のタイトルを編集するイベントリスナー
@@ -69,6 +70,35 @@ const editDetailOff = (detailElm, editDetailElm) => {
     )
 }
 
+//カードの添付表示の要素を取得
+const attachmentTilte = document.getElementById("attachment_title");
+const attachmentMemo = document.getElementById("attachment_memo");
+const attachment = document.getElementById("attachment");
+const picture = document.getElementById("picture");
+
+
+//カードの添付表示をオンにして、カードの要素をコンテナに渡すイベントリスナー
+const attachmentOn = (detailElm, titleText, memoText, img) => {
+    detailElm.addEventListener("click",
+        () => {
+            attachmentTilte.innerText = `${titleText}`;
+            attachmentMemo.innerText = `${memoText}`;
+            picture.src = `${img}`;
+            attachment.style.visibility = "visible";
+        }
+    )
+}
+
+//カードの添付表示をオフにするイベントリスナー
+const backBtn = document.getElementById("back");
+backBtn.addEventListener("click",
+    () => {
+        document.getElementById("attachment").style.visibility = "hidden";
+    }
+)
+
+
+
 
 //要素を作成しクラス名をつける関数
 const createElement = (element, className) => {
@@ -78,7 +108,7 @@ const createElement = (element, className) => {
 }
 
 //カード内の要素を作成するイベントリスナー
-const addDetail = (addDetailsBox, card_detailContainer, card,cardHeight) => {
+const addDetail = (addDetailsBox, card_detailContainer, card, cardHeight) => {
     let clickCounter = 0;
     addDetailsBox.addEventListener("click",
         () => {
@@ -86,13 +116,18 @@ const addDetail = (addDetailsBox, card_detailContainer, card,cardHeight) => {
             const NewCard_detailsBox = createElement("div", "card_detailsBox");
             const NewCard_Details = createElement("h4", "card_details");
             const NewCard_EditDetail = createElement("input", "edit_detail");
+            const NewCard_add = createElement("button", "add");
+            const cardTitle = card.getElementsByClassName("card_title")[0];
             card_detailContainer.appendChild(NewCard_detailsBox);
             NewCard_detailsBox.appendChild(NewCard_Details);
             NewCard_detailsBox.appendChild(NewCard_EditDetail);
+            NewCard_detailsBox.appendChild(NewCard_add);
+            NewCard_add.innerText = "…";
             card_detailContainer.appendChild(addDetailsBox);
             card.style.height = `${cardHeight + (70 * clickCounter)}px`;
             editDetailOn(NewCard_Details, NewCard_EditDetail);
             editDetailOff(NewCard_Details, NewCard_EditDetail);
+            attachmentOn(NewCard_add, cardTitle.innerText, NewCard_Details.innerText);
         }
     )
 
@@ -102,17 +137,18 @@ const addDetail = (addDetailsBox, card_detailContainer, card,cardHeight) => {
 //1枚目のカード内のオブジェクトにイベントリスナーを追加
 editTitleVisible(firstCardTitle, firstCardTitleEdit);
 editTitleVisibleOff(firstCardTitle, firstCardTitleEdit);
-addDetail(firstAddDetailsBox, firstCard_detailContainer, firstCard,150);
+addDetail(firstAddDetailsBox, firstCard_detailContainer, firstCard, 150);
 editDetailOn(firstCardDetails, firstEditDetails);
 editDetailOff(firstCardDetails, firstEditDetails);
+attachmentOn(firstCardAttachment, firstCardTitle.innerText, firstCardDetails.innerText);
+
 
 //カードを配置するchildContainerの要素を取得
 const childContainer = document.getElementById("childContainer");
 const addCardBtn = document.getElementById("card_add");
 
-
 //カードを生成する関数
-const addCard = (memo,title) => {
+const addCard = (memo, title) => {
     //カードの外枠とタイトル、中枠を作成
     const card = createElement("div", "card");
     const card_title = createElement("h2", "card_title");
@@ -120,10 +156,10 @@ const addCard = (memo,title) => {
     card_titleInput.value = "input_title";
     const card_detailContainer = createElement("div", "card_detailContainer");
     //DBのタイトルが引数にあればタイトルに表示
-    if(typeof(title) === "string"){
+    if (typeof (title) === "string") {
         card_title.innerText = `${title}`;
-        card_titleInput.value =  `${title}`;
-    }else {card_title.innerText = "Title"}
+        card_titleInput.value = `${title}`;
+    } else { card_title.innerText = "Title" }
     //作成したタイトルにイベントリスナを設定
     editTitleVisible(card_title, card_titleInput);
     editTitleVisibleOff(card_title, card_titleInput);
@@ -133,46 +169,54 @@ const addCard = (memo,title) => {
     card.appendChild(card_titleInput);
     card.appendChild(card_detailContainer);
     //カードの高さを初期化
-    let cardHeight =0;
+    let cardHeight = 0;
     if (typeof (memo.length) === "number") {
-        for (let i = 0; i <= memo.length-1; i++) {
+        for (let i = 0; i <= memo.length - 1; i++) {
             //子要素を作成
             const card_detailsBox = createElement("div", "card_detailsBox");
             const card_Details = createElement("h4", "card_details");
             const edit_detail = createElement("input", "edit_detail");
+            const add = createElement("button", "add");
+            add.innerText = "…";
             //DBのmemoの要素を表示
-            card_Details.innerText = `${memo[i]}`;
-            edit_detail.value = `${memo[i]}`;
+            card_Details.innerText = `${memo[i].text}`;
+            edit_detail.value = `${memo[i].text}`;
             //子要素にイベントリスナを設定    
             editDetailOn(card_Details, edit_detail);
             editDetailOff(card_Details, edit_detail);
+            attachmentOn(add, card_title.innerText, card_Details.innerText,memo[i].img);
             //子要素を中枠に追加
             card_detailContainer.appendChild(card_detailsBox);
             card_detailsBox.appendChild(card_Details);
             card_detailsBox.appendChild(edit_detail);
-            cardHeight = (i*70);
+            card_detailsBox.appendChild(add);
+            cardHeight = (i * 70);
         }
     } else {
         //子要素を作成
         const card_detailsBox = createElement("div", "card_detailsBox");
         const card_Details = createElement("h4", "card_details");
         const edit_detail = createElement("input", "edit_detail");
+        const add = createElement("button", "add");
+        add.innerText = "…";
         //子要素にイベントリスナを設定    
         editDetailOn(card_Details, edit_detail);
         editDetailOff(card_Details, edit_detail);
+        attachmentOn(add, card_title.innerText, card_Details.innerText);
         //子要素を中枠に追加
         card_detailContainer.appendChild(card_detailsBox);
         card_detailsBox.appendChild(card_Details);
         card_detailsBox.appendChild(edit_detail);
-       
+        card_detailsBox.appendChild(add);
+
     }
-    cardHeight = 150+cardHeight;
+    cardHeight = 150 + cardHeight;
     card.style.height = `${cardHeight}px`;
     //子要素の追加ボタンを作成
     const addDetailsBox = createElement("div", "addDetailsBox");
     const addDetails = createElement("h4", "addDetails");
     //追加ボタンのイベントリスナを設定
-    addDetail(addDetailsBox, card_detailContainer, card,cardHeight);
+    addDetail(addDetailsBox, card_detailContainer, card, cardHeight);
     addDetails.innerText = "+カードの追加";
     //追加ボタンを中枠最後に追加    
     card_detailContainer.appendChild(addDetailsBox);
@@ -190,13 +234,13 @@ const DBswitch = document.getElementsByClassName("DB")[0];
 DBswitch.addEventListener("click",
     () => {
         for (let i = 0; i <= DB.length - 1; i++) {
-            addCard(DB[i].memo,DB[i].title);
+            addCard(DB[i].memo, DB[i].title);
         }
         firstCard.style.visibility = "hidden";
         firstCard.style.width = `${0}px`;
         document.getElementById("menu-list").style.visibility = "visible";
     }
-   
+
 )
 
 
@@ -207,35 +251,42 @@ const DB =
     [
         {
             title: "自己紹介",
-            memo: ["名前 山口智也", "出身地 愛知県豊田市", "住所 名古屋市昭和区",
-                "家族 妻と保護猫2引", "職場 上郷エンジン鋳造部"]
+            memo: [{ text: "名前 山口智也" }, 
+            {text:"趣味 散歩 音楽鑑賞 DJ 料理", img: "IMG_0094.jpg"}
+            { text: "出身地 愛知県豊田市" }, { text: "住所 名古屋市昭和区" },
+            { text: "家族 妻と保護猫2匹" ,img:"IMG_0256.JPG"}, { text: "職場 上郷エンジン鋳造部" }]
         },
         {
             title: "仕事内容",
-            memo: ["現場向けのDXツール制作", "DX関連の困りごと対応、改善"]
+            memo: [{
+                text: "現場向けのDXツール制作",
+                img: "apps1.png"
+            }
+                , { text: "DX関連の困りごと対応、改善" }]
         },
         {
-            title:"なぜDIGを受講したのか",
-            memo: ["PowerPlatformだけで出来ることに限りがあるから",
-                "現場で使うデジタルアイテムは現場で作りたい",
-                "スキルを身に着けて出来ることを増やしたい"
+            title: "なぜDIGを受講したのか",
+            memo: [{ text: "PowerPlatformだけで出来ることに限りがあるから" },
+            { text: "現場で使うデジタルアイテムは現場で作りたい" },
+            { text: "スキルを身に着けて出来ることを増やしたい" }
             ]
         },
         {
-            title:"受講してよかったこと",
-            memo:["一からWebコンテンツを作れるようになった",
-               "プログラミング言語習得の敷居が下がった",
-               "工場外、社外で横の繋がりが出来た"   
+            title: "受講してよかったこと",
+            memo: [{ text: "一からWebコンテンツを作れるようになった" },
+            { text: "プログラミング言語習得の敷居が下がった" },
+            { text: "工場外、社外で横の繋がりが出来た" }
             ]
         },
         {
-            title:"苦戦したこと",
-            memo:["教育と業務の両立"]
+            title: "苦戦したこと",
+            memo: [{ text: "教育と業務の両立" }]
         },
         {
-            title:"今後挑戦したいこと",
-            memo:["DBや外部とのデータの連携",
-            "3Dモデルを扱えるライブラリ(Three.jsや)pytonの物体検知のライブラリなど組み込んだアプリを作りたい",
+            title: "今後挑戦したいこと",
+            memo: [
+                { text: "DBや外部とのデータの連携" },
+                { text: "3Dモデルを扱えるライブラリ(Three.jsや)pytonの物体検知のライブラリなど組み込んだアプリを作りたい" }
             ]
         }
     ]
